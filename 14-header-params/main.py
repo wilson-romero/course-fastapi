7c0da691d5732021938b91d63dcfaf0c313b50d1
Wilson Romero <me@wilsonromero.com>
2025-03-23 12:20:52 -0500

    ✨ header-params

---
+++ 14-header-params/main.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+
+from fastapi import FastAPI, Header
+
+app = FastAPI()
+
+
+# @app.get("/items/")
+# async def read_items(user_agent: Annotated[str | None, Header()] = None):
+#     return {"User-Agent": user_agent}
+
+@app.get("/items/")
+async def read_items(x_token: Annotated[list[str] | None, Header()] = None):
+    return {"X-Token values": x_token}
